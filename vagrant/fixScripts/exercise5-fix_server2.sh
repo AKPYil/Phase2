8c0a6c786f7a3aa3fa63656364e9a1a83a43ac3d
Alek <akpyil@gmail.com>
2019-08-03 16:37:05 +0300

    added all

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+
+cat << 'SSHEOF' > /etc/ssh/ssh_config
+Host *
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+SSHEOF
+
+cat << 'SSHEOF' > /etc/ssh/sshd_config
+AuthorizedKeysFile .ssh/authorized_keys /etc/ssh/authorized_keys
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+UsePAM no
+SSHEOF
+
+cat /vagrant/server1.pub >> /root/.ssh/authorized_keys
+cat /vagrant/server1_vagrant.pub >> /root/.ssh/authorized_keys
+cat /vagrant/server1.pub >> /home/vagrant/.ssh/authorized_keys
+cat /vagrant/server1_vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+cat /vagrant/server1.pub >> /home/ubuntu/.ssh/authorized_keys
+cat /vagrant/server1_vagrant.pub >> /home/ubuntu/.ssh/authorized_keys
--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,48 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+# bash -x /vagrant/fixScripts/exercise6-fix.sh /vagrant/Vagrantfile /etc/crontab /tmp/
+
+if [[ $# < 2 ]];then
+	echo "You need to pass minimum of 2 parameters"
+	exit 1
+fi
+
+remote_path=$BASH_ARGV
+
+if [[ "$HOSTNAME" == "server1" ]]
+then
+    host="server2"
+	command="scp"
+	user=$USER
+else
+    host="server1"
+	user="root"
+	command="sshpass -p vagrant scp"
+	sudo apt-get install sshpass
+fi
+
+for var in ${*%${!#}}
+do
+	if [[ ! -e $var ]]; then
+		echo "Passed file path: '"$var"' does not exist"
+		exit 1
+	fi
+	if [[ ! -d $remote_path ]]; then
+		echo "file output is not defined"
+		exit 1
+	fi
+	
+	$command $var $user@$host:$remote_path
+	
+	if [[ $? != 0 ]]; then
+		echo "*** Error: While copying file '$var' to directory '$remote_path'"
+	fi
+	for file in "$var"*
+		do
+			if [[ -f $file ]]
+			then
+				((size+=$(stat -c%s "$file")))
+			fi
+		done
+done
+echo "$size"
